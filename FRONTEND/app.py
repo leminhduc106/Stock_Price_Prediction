77f2c66a4285a3cef49643ae9ab5520ca67fdb09
nvtphong <nvtphong20042001@gmmail.com>
2023-08-21 22:14:05 +0700

    select model

--- FRONTEND/app.py
+++ FRONTEND/app.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import numpy as np
+import pickle
+
+# from tensorflow.python.keras.layers import Dense, Input
+# from tensorflow.python.keras import Sequential
+# from tensorflow.python.keras.activations import sigmoid
+
+
+from tensorflow.python.keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
 
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
@@ -199,5 +208,190 @@ def update_graph(selected_dropdown_value):
                    'rangeslider': {'visible': True}, 'type': 'date'},
              yaxis={"title":"Transactions Volume"})}
     return figure
+
+
+
+def xgboost_predict_future(data, model, indicatorArr, period):
+    # indicator
+    indicatorArr.insert(1,'volume')
+    
+    # data
+    data = data[indicatorArr]
+    data = data[-2:]
+    
+    # model input
+    X = pd.DataFrame({})
+    n = len(data)
+    for i in range(1, n + 1):
+        for column in data.columns:
+            X[column + '_date_' + str(i)] = [data.iloc[n - i][column]]
+    
+    # predicted value
+    predictedValue = model.predict(X)
+    
+    return predictedValue
+
+def replace_bbands(the_list):
+    for item in the_list:
+        if item == 'KBANDS':
+            yield 'Real Lower Band'
+            yield 'Real Middle Band'
+            yield 'Real Upper Band'
+        else:
+            yield item
+
+
+def lstm_predict_future(data, model, indicatorArr, period):    
+    # data
+    data = data[indicatorArr].values
+    data = data[-60:]
+
+    # scaled data
+    scaler = MinMaxScaler(feature_range=(0,1))
+    scaledData = scaler.fit_transform(data)
+
+    # model input
+    modelInput = scaledData.reshape(-1, scaledData.shape[0], scaledData.shape[1])
+
+    # predicted scaled value
+    predictedScaledValue = model.predict(modelInput)
+
+    # predicted value
+    predictedValue = scaler.inverse_transform(np.tile(predictedScaledValue, (1, scaledData.shape[1])))[:, 0]
+    
+    return predictedValue
+
+
+@app.callback(    
+    Output('predicted_graph', 'figure'),
+               [Input('predict_button', 'n_clicks')], 
+               [
+                   State('dropdown-company', 'value'), 
+                   State('dropdown-model', 'value'),
+                   State('dropdown-indicator', 'value'),
+                   State('dropdown-period', 'value')
+               ]
+              )
+def update_graph(n_clicks, companyName, modelName, indicatorArr, period):
+    data = pd.read_csv("../DATA/" + companyName + '.csv')
+    
+    # model
+    modelFileName = '../MODEL/' + modelName
+            
+    indicatorArr.sort(key = str.lower)
+    
+    for indicator in indicatorArr:
+        if indicator == 'close':
+            continue
+        if indicator == 'KBANDS':
+            indicator = 'BBANDS'
+        modelFileName = modelFileName + '_' + indicator
+        
+    indicatorArr = list(replace_bbands(indicatorArr))
+
+    print(indicatorArr)
+    
+    predictions = None
+    if modelName == 'LSTM' or modelName == 'RNN': 
+        modelFileName = modelFileName + '.h5'
+        model = load_model(modelFileName)
+        futurePredictions = lstm_predict_future(data, model, indicatorArr, period)
+        #
+        dataset = data
+        dataset = dataset[indicatorArr].values
+        scaler = MinMaxScaler(feature_range=(0,1))
+        dataset = scaler.fit_transform(dataset)
+        X = []
+        for i in range(60, len(dataset)):
+            X.append(dataset[i-60:i][:])
+        X = np.array(X[-100:])
+        predictions = model.predict(X)
+        predictions = scaler.inverse_transform(np.tile(predictions, (1, dataset.shape[1])))[:, 0]
+        df = data.iloc[-len(predictions):]
+        df['predictions'] = predictions
+        #
+    elif modelName == 'XGBOOST':
+        modelFileName = modelFileName + '.dat'
+        model = pickle.load(open(modelFileName, "rb"))
+        futurePredictions = xgboost_predict_future(data, model, indicatorArr, period)
+        #
+        dataset = data
+        temp = indicatorArr.copy()
+        dataset = dataset[temp]
+        for i in range (1, 3):
+            for indicator in temp:
+                dataset[indicator + "_date_" + str(i)] = dataset[indicator].shift(i)
+        dataset.dropna(inplace=True)
+        X = dataset.drop(temp, axis=1)
+        X = X[-100:]
+        predictions = model.predict(X)
+        df = data.iloc[-len(predictions):]
+        df['predictions'] = predictions
+        #
+        
+    print(modelFileName)
+        
+    prediction_df = pd.Series(futurePredictions)
+    prediction_df = data['close'].append(pd.Series(futurePredictions))
+    prediction_df = prediction_df.reset_index()
+    prediction_df = prediction_df.drop(columns=['index'], axis=1)
+    prediction_df = prediction_df[0]
+    prediction_df = prediction_df[-len(futurePredictions):]
+
+    
+    figure={
+        "data":[
+            go.Scatter(
+                x=data.index[-300:],
+                y=data.close[-300:],
+                mode='lines',
+                name="Real Price"
+            ),
+            go.Scatter(
+                x=df.index,
+                y=df.predictions,
+                mode='lines',
+                name="Model Validation in 100 previous data points"
+            ),
+            go.Scatter(
+                x=prediction_df.index,
+                y=prediction_df.values,
+                mode='markers',
+                name="Predicted Price"
+            ),
+        ],
+        "layout":go.Layout(
+            title=f"Predicted stock price is {prediction_df.values[0]} USD.",
+            xaxis={'title':'Data Point'},
+            yaxis={'title':'Close Price (USD)'}
+        )
+    }
+    return figure
+
+
+def update_data(companyName):
+    data = ts.get_intraday(symbol=companyName,interval='15min', outputsize='full') # data
+    data = data[0]
+    data.rename(lambda x: x[2:].strip(), axis='columns', inplace=True)
+    indicator_roc = ti.get_roc(symbol=companyName, interval='15min', time_period=20) # roc
+    indicator_roc = indicator_roc[0]
+    indicator_sma = ti.get_sma(symbol=companyName, interval='15min', time_period=20) # sma
+    indicator_sma = indicator_sma[0]
+    indicator_rsi = ti.get_rsi(symbol=companyName, interval='15min', time_period=20) # rsi
+    indicator_rsi = indicator_rsi[0]
+    indicator_bb = ti.get_bbands(symbol=companyName, interval='15min', time_period=20) # bbands
+    indicator_bb = indicator_bb[0]
+    original_df = pd.merge(data, indicator_roc, on='date', how='inner')
+    original_df = pd.merge(original_df, indicator_sma, on='date', how='inner')
+    original_df = pd.merge(original_df, indicator_rsi, on='date', how='inner')
+    original_df = pd.merge(original_df, indicator_bb, on='date', how='inner')
+    original_df = original_df.iloc[::-1]
+    original_df.to_csv('../DATA/' + companyName +'.csv')
+
+@app.callback(Output('something', 'children'), [Input('update_button', 'n_clicks')] )
+def update_output(n_clicks):
+    update_data('MSFT')
+    df = pd.read_csv("../DATA/MSFT.csv")
+
 
 app.run_server(debug=True, port=8080)
